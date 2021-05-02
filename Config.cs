f5ffda36d1c4189f1b04be14969909f2059eea04
CodeAngel <83538964+CodeAngel3@users.noreply.github.com>
2021-05-02 13:53:10 +0200

    Create Config.cs

---
+++ Config.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RipperStoreCreditsUploader
+{
+    public class Config
+    {
+        public string apiKey { get; set; }
+        public bool LogToConsole { get; set; }
+    }
+}
